bcc06590ff78bc0a8aebc1547711dc2003fc39c5
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2022-10-13 23:57:57 +0300

    Web Api - IoC added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]   //ATTRIBUTE
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled (Gevşek Bağımlılık) => Soyuta bağımlılık var.
+        //naming convension => İsimlendirme standartı
+        //IoC Container => İnversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain -- (Bağımlılık zinciri)
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
