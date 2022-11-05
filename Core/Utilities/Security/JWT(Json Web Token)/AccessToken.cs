8cded0ff236f974fb18f5b3ce7ea40dd3aec72a9
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2022-11-05 19:23:50 +0300

    JWT güncellemesi

---
+++ Core/Utilities/Security/JWT(Json Web Token)/AccessToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Utilities.Security.JWT_Json_Web_Token_
+{
+    public class AccessToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; } //tokenin bitiş süresi
+
+    }
+}
---
+++ Core/Utilities/Security/JWT(Json Web Token)/ITokenHelper.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Concrete;
+
+namespace Core.Utilities.Security.JWT_Json_Web_Token_
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+
+    }
+}
